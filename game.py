aaed87acb36df115320f8cc6021b9406279946cf
Nafim_uddin <email@example.com>
2026-01-22 10:49:12 +0100

    prova commit

--- game.py
+++ game.py
@@ -1,1 +1,30 @@
+import arcade
 
+# Costanti che non cambiano mai
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "My First Arcade Window"
+
+def main():
+    # Apri la finestra
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    
+    # Imposta lo sfondo
+    arcade.set_background_color(arcade.color.SKY_BLUE)
+    
+    # Inizia a renderizzare
+    arcade.start_render()
+    
+    # cubo e movimento
+    while True:
+        
+        
+    
+    # Finisci di renderizzare
+    arcade.finish_render()
+    
+    # Fai partire il tutto e mantieni la finestra aperta
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
